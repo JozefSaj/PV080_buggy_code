aae019ecee0080b9a98aed1d9f74dfd52a70d031
Jozef Sajtlava <jozef.sajtlava02@gmail.com>
2023-03-30 10:46:54 +0200

    some basic warning fixed

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,3 @@
-import sys 
 import os
 import yaml
 import flask
@@ -30,7 +29,6 @@ def fetch_website(urllib_version, url):
  
     try: 
         http = urllib.PoolManager()
-        r = http.request('GET', url)
     except:
         print('Exception')
 
@@ -39,7 +37,8 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
